4d5705d7b5f8d695537f231d7a0fcc482fc22170
Christopher Walsh <walshification@gmail.com>
2016-10-30 12:51:18 -0500

    draws that board!

--- pset3/fifteen/fifteen.c
+++ pset3/fifteen/fifteen.c
@@ -196,7 +196,22 @@ void init(void)
  */
 void draw(void)
 {
-    // TODO
+    for (int i = 0; i < d; ++i)
+    {
+        for (int j = 0; j < d; ++j)
+        {
+            if (board[i][j] > 0)
+            {
+                printf("%2d ", board[i][j]);
+            }
+            else
+            {
+                printf(" _ ");
+            }
+        }
+        printf("\n");
+    }
+    printf("\n");
 }
 
 /**
