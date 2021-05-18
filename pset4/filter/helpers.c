290ed4767ea3ea4755bbd94054e56bf9482e1ee7
Christopher Walsh <5120876+walshification@users.noreply.github.com>
2021-05-18 14:59:57 -0500

    Implement grayscale

--- pset4/filter/helpers.c
+++ pset4/filter/helpers.c
@@ -1,8 +1,27 @@
+#include <math.h>
+
 #include "helpers.h"
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; ++i)
+    {
+        for (int j = 0; j < width; ++j)
+        {
+            // Average the current color values.
+            int current_red = image[i][j].rgbtRed;
+            int current_green = image[i][j].rgbtGreen;
+            int current_blue = image[i][j].rgbtBlue;
+            // Be careful to divide by a float and round the result!
+            int average_color = round((current_blue + current_green + current_red) / 3.0);
+
+            // Set each color to the average value for gray.
+            image[i][j].rgbtRed = average_color;
+            image[i][j].rgbtGreen = average_color;
+            image[i][j].rgbtBlue = average_color;
+        }
+    }
     return;
 }
 
